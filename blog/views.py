bd32b259e0aa6f88b25a1b7852ba9533a15439ce
Aniket762 <aniketindian8@gmail.com>
2020-12-31 10:41:29 +0530

    Post Detail

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
 from .models import Post
-
+from django.views.generic import ListView, DetailView
+#list view gives the youTube wala feel
 
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostViewList(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    # by default it expects <app>/<model>_<viewtype>.html
+    # template passing is basically the page it needs to show when func called
+    context_object_name = 'posts' # object to loop over
+    ordering = ['-date_posted'] 
+    #ordering the post on basis of time, bringing the latest post top
+
+
+# taki har ek post k lia ek page ban jai, url mei khelna hai 
+class PostDetailView(DetailView):
+    model = Post
+    
 
 
 def about(request):
